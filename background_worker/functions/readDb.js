2ace11dc29641772a99eaacd5fa69766cf32b035
AD\petar.korudzhiev <petar.korudzhiev@mentormate.com>
2018-04-24 13:50:04 +0300

    try lambda from VPC

--- background_worker/functions/readDb.js
+++ background_worker/functions/readDb.js
@@ -1,4 +1,5 @@
 import * as snsLib from '../../libs/sns-lib'
+import * as dynamoDbLib from '../../libs/dynamodb-lib'
 import { success, failure } from '../../libs/response-lib'
 
 export async function main(event, context, callback) {
@@ -6,6 +7,18 @@ export async function main(event, context, callback) {
     try {
         const message = event.Records[0].Sns.Message;
         console.log('From SNS: ', message);
+
+
+        var params = {
+            Item: {
+             "id":  "1", 
+             "name":  "Petar", 
+             "description": "Korudzhiev"
+            }, 
+            TableName: process.env.ADVERTISEMENTS_TABLE_NAME
+        };
+
+        await dynamoDbLib.call("put", params)   
 
         const message2 = { 
             name: 'read from db' 
