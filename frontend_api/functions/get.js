fd7ad8c03d6f4771e3d96d532fa73444dd32cb4f
AD\petar.korudzhiev <petar.korudzhiev@mentormate.com>
2018-04-30 16:30:53 +0300

    implement return random ads

--- frontend_api/functions/get.js
+++ frontend_api/functions/get.js
@@ -1,24 +1,52 @@
 import * as dynamoDbLib from '../../libs/dynamodb-lib'
 import { success, failure } from '../../libs/response-lib'
 
+const defaultRows = 3;
+
 export async function main(event, context, callback) {
+  let adsRows = defaultRows;
+  if(event.queryStringParameters && event.queryStringParameters.hasOwnProperty('adsRows')){
+    adsRows = event["queryStringParameters"]['adsRows']
+  }
+
+  const adsCount = 4; // get from config
+
+  if (adsRows <= 0) {
+    adsRows = defaultRows
+  }
+
+  if (adsRows > adsCount) {
+    adsRows = adsCount;
+  }
+
+  let numbers = []
+  while(numbers.length < adsRows){
+      var randomnumber = Math.floor(Math.random() * adsCount) + 1;
+      if(numbers.indexOf(randomnumber) > -1) continue;
+      numbers[numbers.length] = randomnumber;
+  }
+
+  let keys = [];
+  var i;
+  for (i = 0; i < numbers.length; i++) { 
+    keys.push({ id: '1_42342342334_' + numbers[i] })
+  }
 
   const params = {};
   params.RequestItems = {};
   params.RequestItems[process.env.ADVERTISEMENTS_TABLE_NAME] = {
-    Keys: [
-      { id: '1_42342342334_4' },
-      { id: '1_42342342334_1' }
-    ]
+    Keys: keys
   };
-
-  console.log(params);
 
   try {
     const result = await dynamoDbLib.call("batchGet", params);
 
-    callback(null, success(result));
-
+    if (result.Responses) {
+      callback(null, success(result.Responses[process.env.ADVERTISEMENTS_TABLE_NAME]));
+    }
+    else {
+      callback(null, failure({ status: false }));
+    }
   } catch (e) {
     console.log(e);
     callback(null, failure({ status: false }));
